db9c2b563f40c56d603dc56b8960ea62979c32b7
Arunav Gupta <arunavgupta@Arunavs-MacBook-Pro.local>
2019-01-05 13:59:34 -0800

    add skeleton code for main gamplay

---
+++ GamePlay.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    //set characters
+    //set item amounts, etc
+    while(/*at least one player's health > 0, and distance to finish > 0*/)
+    {
+        //option to travel
+        //obstacles per "turn"
+    }
+
+    if (/*died*/)
+    {
+        cout << "You died along the journey at " << /*print wherever the last person died*/ << endl;
+    } else if (/*distance to finish = 0*/)
+    {
+        cout << "Congratulations! You have made it to Oregon! Let's see how many points you have recieved." << endl;
+        //print date of arrival
+        //total and print number of points earned
+    }
+}
