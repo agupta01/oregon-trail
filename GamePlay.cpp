d1f32dd1fcfa1d5bd7638fc92567c2b07849b2ce
Arunav Gupta <arunavgupta@Arunavs-MacBook-Pro.local>
2019-01-05 16:34:19 -0800

    add obstacle struct

--- GamePlay.cpp
+++ GamePlay.cpp
@@ -6,14 +6,87 @@ int money = 2000;
 int distance = 1600;
 int ammo;
 int food;
+
+struct Event
+{
+    int dDist;
+    int dHealth;
+    int dAmmo;
+    int dMoney;
+    int dFood;
+};
+
 struct Person{
     string name; 
     int health;
 };
+
+int a[10][5]={{0,-2,0,0,0},
+              {0,0,0,0,-150},  
+              {0,-1,0,0,0},  
+              {0,0,0,0,-100},  
+              {0,0,-10,0,0},  
+              {0,0,-20,0,0},  
+              {0,0,0,-200,0},  
+              {0,0,0,-50,0},  
+              {-50,0,0,0,0},  
+              {-10,0,0,0,0}};
+
+int b[10][5]={{0,-2,0,0,0},
+              {0,0,0,0,-500},  
+              {0,-1,0,0,0},  
+              {0,0,0,0,-600},  
+              {0,0,-5,0,0},  
+              {0,0,-10,0,0},  
+              {0,0,0,0,-100},  
+              {0,0,0,-300,0},  
+              {-150,0,0,0,0},  
+              {-50,0,0,0,0}};
+
+int c[10][5]={{0,0,-20,0,0},
+              {-100,0,0,0,0},  
+              {0,-2,0,0,0},  
+              {0,-3,0,0,0},  
+              {0,0,-30,0,0},  
+              {-10,0,0,0,0},  
+              {0,0,0,-25,0},  
+              {0,0,0,-250,0},  
+              {0,0,0,0,-100},  
+              {0,0,0,0,-30}};
+
 int main()
 {
     
-    cout<<"Welcome to the Oregon-Trail Mission San Jose Edition"<<endl; 
+    cout<<"Welcome to the Oregon-Trail Mission San Jose Edition"<<endl;
+    Event missionCreek[2];
+    for(int i = 0; i < 2; i++)
+    {
+        missionCreek[i].dDist = a[i][0];
+        missionCreek[i].dHealth = a[i][1];
+        missionCreek[i].dAmmo = a[i][2];
+        missionCreek[i].dMoney = a[i][3];
+        missionCreek[i].dFood = a[i][4];
+    }
+    
+    Event elitePrep[2];
+    for(int i = 0; i < sizeof(b); i++)
+    {
+        elitePrep[i].dDist = b[i][0];
+        elitePrep[i].dHealth = b[i][1];
+        elitePrep[i].dAmmo = b[i][2];
+        elitePrep[i].dMoney = b[i][3];
+        elitePrep[i].dFood = b[i][4];
+    }
+
+    Event missionPeak[2];
+    for(int i = 0; i < sizeof(c); i++)
+    {
+        missionPeak[i].dDist = c[i][0];
+        missionPeak[i].dHealth = c[i][1];
+        missionPeak[i].dAmmo = c[i][2];
+        missionPeak[i].dMoney = c[i][3];
+        missionPeak[i].dFood = c[i][4];
+    } 
     Person players[5];
     for (int i =0; i< 5; i++)
     {
