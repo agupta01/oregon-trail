1c2c9e3b19c84ffd802a84e62dab9d94cdaf79b1
Arunav Gupta <centaurus.ab@gmail.com>
2019-01-08 08:24:38 -0800

    split init into new method, big fixes

--- GamePlay.cpp
+++ GamePlay.cpp
@@ -6,7 +6,7 @@ int money = 2000;
 int distance = 1600;
 int ammo;
 int food;
-//aruaruaruaru
+
 struct Event
 {
     int dDist;
@@ -53,79 +53,97 @@ int c[10][5]={{0,0,-20,0,0},
               {0,0,0,-250,0},  
               {0,0,0,0,-100},  
               {0,0,0,0,-30}};
+Event missionCreek[2];
+Event elitePrep[2];
+Event missionPeak[2];
+Person players[5];
 
-int main()
+void init()
 {
-    
     cout<<"Welcome to the Oregon-Trail Mission San Jose Edition"<<endl;
-    Event missionCreek[2];
-    for(int i = 0; i < 2; i++)
+    
+    //create obstacle classes
+    for(int i = 0; i < 10; i++)
     {
         missionCreek[i].dDist = a[i][0];
         missionCreek[i].dHealth = a[i][1];
         missionCreek[i].dAmmo = a[i][2];
         missionCreek[i].dMoney = a[i][3];
         missionCreek[i].dFood = a[i][4];
+        //cout << "Done copying a" << endl;
     }
     
-    Event elitePrep[2];
-    for(int i = 0; i < sizeof(b); i++)
+    
+    for(int i = 0; i < 10; i++)
     {
         elitePrep[i].dDist = b[i][0];
         elitePrep[i].dHealth = b[i][1];
         elitePrep[i].dAmmo = b[i][2];
         elitePrep[i].dMoney = b[i][3];
         elitePrep[i].dFood = b[i][4];
+        //cout << "Done copying b" << endl;
+
     }
 
-    Event missionPeak[2];
-    for(int i = 0; i < sizeof(c); i++)
+    
+    for(int i = 0; i < 10; i++)
     {
         missionPeak[i].dDist = c[i][0];
         missionPeak[i].dHealth = c[i][1];
         missionPeak[i].dAmmo = c[i][2];
         missionPeak[i].dMoney = c[i][3];
         missionPeak[i].dFood = c[i][4];
+        //cout << "Done copying c" << endl;
+
     } 
-    Person players[5];
-    for (int i =0; i< 5; i++)
+    
+    //create players
+    for (int i = 0; i< 5; i++)
     {
-        cout<<"What is player "<< i<< " name?"<<endl; 
+        cout<<"What is player "<< i + 1 << "'s name?"<<endl; 
         string name; 
         cin >> name;
-        players[i] = Person{name, 10};
+        players[i].name = name;
+        players[i].health = 10;
     }
-    cout<<"You will be embarking on a 1600 mile journey for 30 days, where only the fittest will survive"<<endl;
-    cout<<"Before you begin you must buy ammo and food with your starting $2000"<<endl; 
-    cout<<"Ammo costs $10 while food costs $20"<<endl; 
-    cout<<"How much ammo would you like to buy?"<<endl; 
+}
+
+int main()
+{
+    init();
+
+    cout << "You will be embarking on a 1600 mile journey for 30 days, where only the fittest will survive" << endl;
+    cout << "Before you begin you must buy ammo and food with your starting $2000" << endl; 
+    cout << "Ammo costs $10 while food costs $20" << endl; 
+    cout << "How much ammo would you like to buy?" << endl; 
     int ammoBought; 
-    cin>> ammoBought;
+    cin >> ammoBought;
     
-        if(cin.fail())
+    if(cin.fail())
     {  
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(),
         '\n');
-        cout<<"You must enter an integer value "<<endl;
+        cout << "You must enter an integer value " << endl;
         cin>>ammoBought;
     }
     while(10 * ammoBought > money)
     {
         cout<<"This amount is too much, please enter another amount"<<endl;
         cin>>ammoBought;
+
         if(cin.fail())
-    {  
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),
-        '\n');
-        cout<<"You must enter an integer value "<<endl;
-        cin>>ammoBought;
-    }
+        {  
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),
+            '\n');
+            cout<<"You must enter an integer value "<<endl;
+            cin>>ammoBought;
+        }
     }
     ammo += ammoBought;
     money -= ammoBought *10;
-    cout << "How much food would you like to buy? Food costs $20"
+    cout << "How much food would you like to buy? Food costs $20" << endl;
     int foodBought;
     cin >> foodBought;
     if(cin.fail())
@@ -141,53 +159,33 @@ int main()
         cout<<"This amount is too much, please enter another amount"<<endl;
         cin>>foodBought;
         if(cin.fail())
-    {  
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),
-        '\n');
-        cout<<"You must enter an integer value "<<endl;
-        cin>>ammoBought;
-    }
+        {  
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),
+            '\n');
+            cout<<"You must enter an integer value "<<endl;
+            cin>>ammoBought;
+        }
     }
     food +=foodBought;
-    money -= foodBought*20;
-    
-    
-
-    int foodBought; 
-    cin>>foodBought;
-     while(1)
-    {
-        if(cin.fail())
-    {  
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),
-        '\n');
-        cout<<"You have entered wrong input"<<endl;
-        cin>>ammoBought;
-    }
-
-if(!cin.fail())
-break;
-}
-    cout<<"How much food would you like to buy?"<<endl; 
+    money -= foodBought*20; 
 
     
-    //set characters
-    //set item amounts, etc
-    while(/*at least one player's health > 0, and distance to finish > 0*/)
-    {
-        //option to travel
-        //obstacles per "turn"
-    }
+    // //set characters
+    // //set item amounts, etc
+    // while(/*at least one player's health > 0, and distance to finish > 0*/)
+    // {
+    //     //option to travel
+    //     //obstacles per "turn"
+    // }
 
-    if (/*died*/)
-    {
-        cout << "You died along the journey at " << /*print wherever the last person died*/ << endl;
-    } else if (/*distance to finish = 0*/)
-    {
-        cout << "Congratulations! You have made it to Oregon! Let's see how many points you have recieved." << endl;
-        //print date of arrival
-        //total and print number of points earned
-    }
+    // if (/*died*/)
+    // {
+    //     cout << "You died along the journey at " << print wherever the last person died << endl;
+    // } else if (/*distance to finish = 0*/)
+    // {
+    //     cout << "Congratulations! You have made it to Oregon! Let's see how many points you have recieved." << endl;
+    //     //print date of arrival
+    //     //total and print number of points earned
+    // }
 }
