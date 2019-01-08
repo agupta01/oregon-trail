8639820d523c51ddda6c2897aee9057ff536d84b
Arunav Gupta <centaurus.ab@gmail.com>
2019-01-08 12:18:11 -0800

    Add store to game loop

--- GamePlay.cpp
+++ GamePlay.cpp
@@ -9,7 +9,7 @@ int money = 2000;
 int distToTravel = 1600;
 int ammo;
 int food;
-int days = 0;
+int days = 1;
 
 struct Event
 {
@@ -64,9 +64,7 @@ Event missionPeak[2];
 Person players[5];
 
 void init()
-{
-    cout << "Welcome to the Oregon-Trail Mission San Jose Edition" << endl;
-    
+{   
     //create obstacle classes
     for(int i = 0; i < 10; i++)
     {
@@ -115,8 +113,8 @@ void init()
 
 void store()
 {
-    cout << "You will be embarking on a 1600 mile journey for 30 days, where only the fittest will survive" << endl;
-    cout << "Before you begin you must buy ammo and food with your starting $2000" << endl; 
+    cout << "WELCOME TO THE STORE" << endl;
+    cout << "You must buy ammo and food with your starting $2000" << endl; 
     cout << "Ammo costs $10 while food costs $20" << endl; 
     cout << "How much ammo would you like to buy?" << endl; 
     int ammoBought; 
@@ -145,8 +143,11 @@ void store()
         }
     }
     ammo += ammoBought;
-    money -= ammoBought *10;
-    cout << "How much food would you like to buy? Food costs $20" << endl;
+    money -= ammoBought * 10;
+
+    cout << "Money left: $" << money << endl;
+
+    cout << "How much food would you like to buy? Food costs $10, and each person consumes 1 food per day" << endl;
     int foodBought;
     cin >> foodBought;
     if(cin.fail())
@@ -157,7 +158,7 @@ void store()
         cout<<"You must enter an integer value "<<endl;
         cin>>foodBought;
     }
-    while(foodBought*20>money)
+    while(foodBought * 10 > money)
     {
         cout<<"This amount is too much, please enter another amount"<<endl;
         cin>>foodBought;
@@ -167,11 +168,11 @@ void store()
             cin.ignore(numeric_limits<streamsize>::max(),
             '\n');
             cout<<"You must enter an integer value "<<endl;
-            cin>>ammoBought;
+            cin>>foodBought;
         }
     }
     food += foodBought;
-    money -= foodBought*20; 
+    money -= foodBought * 10; 
 
     cout << "You have $" << money << " left." << endl;
     cout << "Items bought:" << endl;
@@ -183,20 +184,28 @@ void store()
 
 bool gameOn()
 {
+    int dead = 0;
+
     for(int i = 0; i < 5; i++)
     {
-        if (players[i].health <= 0)
+        if (players[i].health <= 1)
         {   
-            cout << "You died along the journey on day " << days << endl;
-            return false;
+            cout << players[i].name << " died along the journey on day " << days << " due to bad health." << endl;
+            dead++;
         }
+    }
+
+    if (dead == 5)
+    {
+        cout << "Everyone on the journey has died and the game is over." << endl;
+        return false;
     }
 
     if (distToTravel <= 0)
     {
         cout << "Contratulations! You have made it to Oregon! Let's see how many points you have recieved." << endl;
         return false;
-    } else if (days >= 30)
+    } else if (days > 30)
     {
         cout << "Your journey has now surpassed 30 days and everyone alive has contracted dysentery. You are all doomed to die." << endl;
         return false;
@@ -205,31 +214,104 @@ bool gameOn()
     return true;
 }
 
+void updateValues(int dDist, int dHealth, int dAmmo, int dMoney, int dFood) //NOTE: dFood is total amount req'd for the day by one player
+{   
+    distToTravel += dDist;
+    for (int i = 0; i < 5; i++)
+    {    
+        //if no food left, takes 1 away from each unfed players' health
+        if (food <= 0)
+        {
+            cout << "You are out of food. " << players[i].name << "'s health will drop by 1 every day until they are fed." << endl;
+            players[i].health--;
+        } else {
+            food -= dFood;
+        }
+    }
 
+    days++;
+
+    cout << "Distance left to travel: " << distToTravel << endl;
+    cout << "$" << money << endl;
+    cout << "Ammo Left: " << ammo << endl;
+    cout << "Food Left: " << food << endl;
+    cout << "Player Health Levels:" << endl;
+    for (Person x: players)
+    {
+        cout << x.name << " - " << x.health << endl;
+    }
+}
 
 int main()
 {
+    srand (time(NULL));
+
+    cout << "Welcome to Oregon Trail: MSJ Edition! You will be embarking on a 1600 mile journey for 30 days, where only the fittest will survive." << endl;
+
     init();
     store();
 
    
     while(gameOn())
     {
-        int action = rand() % 3 + 1;
-        cout << action;
+        cout << endl << "------- DAY " << days << " -------" << endl;
+        int action = rand() % 4 + 1;
+        cout << action << endl;
+        string ct;
         switch(action)
         {
+            //no event, travel 80 miles
             case 1:
-            days++;
-            break;
+                cout << "Nothing happened today, but you traveled 80 miles!" << endl;
+                updateValues(-80, 0, 0, 0, 1);
+                cout << "Press any key to continue." << endl;
+                cin >> ct;
+                if(!cin.fail())
+                {
+                    break;
+                } else {
+                    cout << "Nice.";
+                    break;
+                }
             case 2:
-            days++;
-            break;
+                updateValues(0,0,0,0,1);
+                cout << "Press any key to continue." << endl;
+                cin >> ct;
+                if(!cin.fail())
+                {
+                    break;
+                } else {
+                    cout << "Nice.";
+                    break;
+                }
             case 3:
-            days++;
-            break;
+                updateValues(0,0,0,0,1);
+                cout << "Press any key to continue." << endl;
+                cin >> ct;
+                if(!cin.fail())
+                {
+                    break;
+                } else {
+                    cout << "Nice.";
+                    break;
+                }
+            case 4:
+                cout << "You have chanced open a store in the wilderness! If you would not like to buy anything, you do not have to." << endl;
+                store();
+                updateValues(-80, 0, 0, 0, 1);
+                cout << "Press any key to continue." << endl;
+                cin >> ct;
+                if(!cin.fail())
+                {
+                    break;
+                } else {
+                    cout << "Nice.";
+                    break;
+                }
+
         }
-        //option to travel
         //obstacles per "turn"
     }
+
+    return 0;
 }
